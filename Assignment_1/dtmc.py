93ba70816fa9df38aceb1ee66d49a3d2a15dce16
Torstein Heltne Hovde <torstein.hovde@gmail.com>
2022-02-08 21:42:22 +0100

    completed task 9,10,11 and 12

--- Assignment_1/dtmc.py
+++ Assignment_1/dtmc.py
@@ -6,7 +6,7 @@ from random import seed
 from random import random
 import matplotlib.pyplot as plt
 
-# Data structure
+# Data Structure
 
 class DTMC:
 
@@ -78,67 +78,145 @@ class Manage:
         self.name=name
         self.dtmc=dtmc
         self.probability_dist=probability_dist
-    
+        self.transitionMatrix = probability_dist.getStatesProbabilites()
+        self.states = dtmc.getStates()
+        self.initialState = probability_dist.getInitialState()
+        self.transitions=dtmc.getTransitions()
+        
+
 
     def writeToFile(self, filename, dtmc, probability_dist):
         states = dtmc.getStates()
-        probability_states = probability_dist.getStatesProbabilites()
+        #probability_states = probability_dist.getStatesProbabilites()
         initial_state = probability_dist.getInitialState()
 
         f = open(filename, 'w')
         f.write('MarkovChain ' + dtmc.getName() + '\n')
-        f.write("  " + states[0] + ' -> ' + states[1] + ': ' + str(probability_states[0][1]) + '; \n')
-        f.write("  " + states[1] + ' -> ' + states[0] + ': ' + str(probability_states[1][0]) + '; \n')
-        f.write("  " + states[1] + ' -> ' + states[2] + ': ' + str(probability_states[1][2]) + '; \n')
-        f.write("  " + states[2] + ' -> ' + states[1] + ': ' + str(probability_states[2][1]) + '; \n')
+        f.write("  " + states[0] + ' -> ' + states[1] + ': ' + str(self.transitionMatrix[0][1]) + '; \n')
+        f.write("  " + states[1] + ' -> ' + states[0] + ': ' + str(self.transitionMatrix[1][0]) + '; \n')
+        f.write("  " + states[1] + ' -> ' + states[2] + ': ' + str(self.transitionMatrix[1][2]) + '; \n')
+        f.write("  " + states[2] + ' -> ' + states[1] + ': ' + str(self.transitionMatrix[2][1]) + '; \n')
 
         f.write("\n")
         f.write('ProbabilityDistribution p0 of ' + dtmc.getName() + '\n')
-        f.write('  ' + states[0]+"   "+str(initial_state[0]) + '; \n')
-        f.write('  ' + states[1]+":  "+str(initial_state[1]) + '; \n')
-        f.write('  ' + states[2]+":  "+str(initial_state[2]) + '; \n')
+        f.write('  ' + states[0]+"   "+str(self.initialState[0]) + '; \n')
+        f.write('  ' + states[1]+":  "+str(self.initialState[1]) + '; \n')
+        f.write('  ' + states[2]+":  "+str(self.initialState[2]) + '; \n')
         f.close()
 
+    #Task 9
     def checkMarkovChain(self, dtmc, probability_dist):
 
-        transitionMatrix = probability_dist.getStatesProbabilites()
-        states = dtmc.getStates()
         correct = True
-        
-        #Checking sum:
-        if sum(transitionMatrix[0]) + sum(transitionMatrix[1]) + sum(transitionMatrix[2]) != 3:
+
+        #Sjekk at initialvalues ikke er større enn 1:
+        if sum(self.initialState)!=1:
+            print("The sum of the initialvalues have to be equal to one")
+
+        #Checking sum
+        if (sum(self.transitionMatrix[0]) or sum(self.transitionMatrix[1]) or sum(self.transitionMatrix[2])) != 1:
             print("The sum of the probabilities is not correct!")
+            correct = False 
+
+
+         #Checking that CR and RC are not greater than 0.0. IF NEEDED
+        '''if (transitionMatrix[0][2]):
+            print("CR can not be greater than 0.0")
             correct = False
-            
-        #Cheking in states:
-        if (transitionMatrix[1][0] == 0 and transitionMatrix[2][0] == 0):
+
+        if (transitionMatrix[2][0] > 0):
+            print("RC can not be greater than 0.0")
+            correct = False'''  
+
+
+        #Checking in states:
+        if (self.transitionMatrix[1][0] == 0 and self.transitionMatrix[2][0] == 0):
             print("You do not have in states for:", states[0])
             correct = False
 
-        if (transitionMatrix[0][1] == 0 and transitionMatrix[2][1] == 0):
+        if (self.transitionMatrix[0][1] == 0 and self.transitionMatrix[2][1] == 0):
             print("You do not have in states for:", states[1])
             correct = False
         
-        if (transitionMatrix[0][2] == 0 and transitionMatrix[1][2] == 0):
+        if (self.transitionMatrix[0][2] == 0 and self.transitionMatrix[1][2] == 0):
             print("You do not have in states for:", states[2])
             correct = False
+
+       
             
-        #Cheking outstates:
-        if (transitionMatrix[0][1] == 0 and transitionMatrix[0][2] == 0):
+        #Checking outstates:
+        if (self.transitionMatrix[0][1] == 0 and self.transitionMatrix[0][2] == 0):
             print("You do not have out states for:", states[0])
             correct = False
 
-        if (transitionMatrix[1][0] == 0 and transitionMatrix[1][2] == 0):
+        if (self.transitionMatrix[1][0] == 0 and self.transitionMatrix[1][2] == 0):
             print("You do not have out states for:", states[1])
             correct = False
 
-        if (transitionMatrix[2][0] == 0 and transitionMatrix[2][1] == 0):
+        if (self.transitionMatrix[2][0] == 0 and self.transitionMatrix[2][1] == 0):
             print("You do not have out states for:", states[2])
             correct = False
 
         if correct:
             print("Congratulations, the DTMC and the probability distribution has feasible values.")
 
+
+#CALCULATIONS
+    #Task 10
+    
+    def calcProbDist(self):
+
+        it = 0
+        check = np.array([0, 0, 0])
+        prob = np.dot(self.initialState,self.transitionMatrix)
+       
+        while not np.array_equal(check, prob):
+            check = prob
+            prob = np.round(np.dot(prob,self.transitionMatrix), decimals=4)
+            it+=1
+        print("Etter " + str(it) + " iterasjoner konvergerte sannsynligheten. Sannsynlighetsfordelingen ble: \n" + str(prob))    
+        #return prob
+
+    #Task 11
+    
+    def calcProbDistContinous(self, iterations):
+
+        prob_progression = []
+        distr_hist = [[0,0,0]]
+        
+        prob = np.dot(self.initialState,self.transitionMatrix)
+        for x in range(1,iterations+1):
+            prob = np.round(np.dot(prob,self.transitionMatrix), decimals=4)
+            prob_progression.append(prob)
+            print("The probability distribution after "+str(x)+" steps is: "+str(prob))
+
+        return prob_progression
+
+
+#Time Series
+
+#Task 12
+    def timeseries(self, numberOfStates):
+
+        print("Start state: "+ str(self.states[0]))
+        timeseries = []
+        situationNow="CALM"
+        for i in range(numberOfStates):
+            if situationNow=="CALM":
+                nextStateOptions=[self.states[0]]*6+[self.states[1]]*4
+                change=random.choice(nextStateOptions)
+            elif situationNow=="MODERATE":
+                nextStateOptions=[self.states[0]]*6+[self.states[1]]*3+[self.states[2]]*1
+                change=random.choice(nextStateOptions)
+            else:
+                nextStateOptions=[self.states[1]]*9+[self.states[2]]*1
+                change=random.choice(nextStateOptions)
+            timeseries.append(change)
+            situationNow=change
+        print("After "+str(numberOfStates)+" sequenses of states we end up with the follow timeseries: ")
+        return timeseries
+            
+                    
 #Task 4:
 
 seaCondition = DTMC("seaCondition", 
@@ -154,6 +232,10 @@ seaConditionProbability = probability_dist('seaCondition',
                                             [0.0, 0.9, 0.1]])
 
 seaConditionManage = Manage('seaCondition', seaCondition, seaConditionProbability)
-seaConditionManage.writeToFile('seaCondition.txt', seaCondition, seaConditionProbability)
+seaConditionManage.writeToFile('seaCondition.txt', seaCondition, seaConditionProbability) 
 seaConditionManage.checkMarkovChain(seaCondition, seaConditionProbability)
+seaConditionManage.calcProbDist()
+seaConditionManage.calcProbDistContinous(15)
+print(seaConditionManage.timeseries(100))
+
 
